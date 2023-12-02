d982537b3fd0a3807014f1777626abab64cffa93
Andrew Goacher <me@goach.email>
2023-12-02 13:37:20 +0000

    Add day 2 part 1 test input

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,10 @@ pub fn solve_day_1_part_2(lines: Vec<String>) -> i32 {
         .sum()
 }
 
+pub fn solve_day_2_part_1(lines: Vec<String>) -> i32 {
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -46,6 +50,25 @@ mod tests {
         let expected_result = 281;
 
         let result = solve_day_1_part_2(input);
+        assert_eq!(expected_result, result)
+    }
+
+    #[test]
+    fn solve_day_2_part_1_with_example_input() {
+        let input = vec![
+            String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
+            String::from("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"),
+            String::from(
+                "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            ),
+            String::from(
+                "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            ),
+            String::from("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"),
+        ];
+
+        let expected_result = 8;
+        let result = solve_day_2_part_1(input);
         assert_eq!(expected_result, result)
     }
 
