8babb58bfe52b1c12ce96fdfd34895b9d4106efe
Andrew Goacher <me@goach.email>
2023-12-04 18:00:07 +0000

    Add test example input

--- src/lib.rs
+++ src/lib.rs
@@ -55,6 +55,10 @@ pub fn solve_day_3_part_2(lines: Vec<String>) -> u32 {
         .sum()
 }
 
+pub fn solve_day_4_part_1(lines: Vec<String>) -> u32 {
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -165,6 +169,23 @@ mod tests {
 
         let expected_result = 467835;
         let result = solve_day_3_part_2(input);
+
+        assert_eq!(expected_result, result)
+    }
+
+    #[test]
+    fn solve_day_4_part_1_with_example_input() {
+        let input = vec![
+            String::from("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"),
+            String::from("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"),
+            String::from("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"),
+            String::from("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83"),
+            String::from("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"),
+            String::from("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"),
+        ];
+
+        let expected_result = 13;
+        let result = solve_day_4_part_1(input);
 
         assert_eq!(expected_result, result)
     }
